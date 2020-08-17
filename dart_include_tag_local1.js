b6616e26c979a4169a3130ac5b1cfb87cb7dcce0
BibiDia <sander_de_bruin27@hotmail.com>
2020-08-17 12:28:07 +0200

    Update dart_include_tag_local1.js

--- dart_include_tag_local1.js
+++ dart_include_tag_local1.js
@@ -1,6 +1,11 @@
 var gptadslots = [];
 var googletag = googletag || {};
 googletag.cmd = googletag.cmd || [];
+
+googletag.cmd.push(function() { 
+    googletag.pubads().disableInitialLoad(); 
+});
+
 (function () {
   var gads = document.createElement('script');
   gads.async = true;
@@ -10,6 +15,16 @@ googletag.cmd = googletag.cmd || [];
   var node = document.getElementsByTagName('script')[0];
   node.parentNode.insertBefore(gads, node);
 })();
+
+(function () {
+  var adh = document.createElement('script');
+  adh.async = true;
+  adh.type = 'text/javascript';
+  adh.src = 'https://www.pool-vakmedianet.adhese.com/tag/config.js';
+  var node = document.getElementsByTagName('script')[0];
+  node.parentNode.insertBefore(adh, node);
+})();
+
 function ConsentCheck(cctype) {
   return true;
 }
